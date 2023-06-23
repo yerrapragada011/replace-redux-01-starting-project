23acf125a4bc224438d894f7f1507b9f35581f0b
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-06-23 13:51:44 -0400

    Using the Custom Store

--- src/hooks-store/products-store.js
+++ src/hooks-store/products-store.js
@@ -3,11 +3,11 @@ import { initStore } from './store';
 const configureStore = () => {
   const actions = {
     TOGGLE_FAV: (curState, productId) => {
-      const prodIndex = curState.findIndex((p) => p.id === productId);
-      const newFavStatus = !curState[prodIndex].isFavorite;
-      const updatedProducts = [...curState];
+      const prodIndex = curState.products.findIndex((p) => p.id === productId);
+      const newFavStatus = !curState.products[prodIndex].isFavorite;
+      const updatedProducts = [...curState.products];
       updatedProducts[prodIndex] = {
-        ...curState[prodIndex],
+        ...curState.products[prodIndex],
         isFavorite: newFavStatus,
       };
       return { products: updatedProducts };
